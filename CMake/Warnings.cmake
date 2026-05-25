0b627dceade495aa28ca325f289dbc15f45c2aea
eyalamir <eyalamirmusic@gmail.com>
2026-05-24 18:08:32 -0700

    Warnings: Fixed warnings for clang-cl

--- CMake/Warnings.cmake
+++ CMake/Warnings.cmake
@@ -1,8 +1,8 @@
 add_library(miro_warnings INTERFACE)
 
-if (MSVC)
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     target_compile_options(miro_warnings INTERFACE /W4)
-else ()
+elseif (NOT MSVC)
     target_compile_options(miro_warnings INTERFACE
             -Wall -Wextra -Wpedantic)
 endif ()
